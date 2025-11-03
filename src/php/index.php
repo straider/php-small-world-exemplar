666a8a2a449d9e27bc741d184c18a2bc320abd57
josem <JoseM@mo.co.uk>
2025-11-03 16:14:07 +0000

    Refined exemplar index PHP content, to better debug MySQL issues.

--- src/php/index.php
+++ src/php/index.php
@@ -7,15 +7,20 @@ $database   = "demo";
 // Connect to MySQL
 $conn = new mysqli( $servername, $username, $password, $database );
 if ( $conn->connect_error ) {
-    die( "Connection failed: " . $conn->connect_error );
+    die( "<p style='color:red'>Connection failed: " . $conn->connect_error . "</p>" );
 }
 
 echo "<h1>Hello, World from PHP 5.6 + MySQL!</h1>";
 
 // Simple query
-$result = $conn->query( "SELECT NOW() AS 'current_time'" );
-$row    = $result->fetch_assoc();
-echo "<p>Current time from MySQL: " . $row[ 'current_time' ] . "</p>";
+$sql = "SELECT NOW() AS 'current_time'";
+if ( $result = $conn->query( $sql ) ) {
+    $row = $result->fetch_assoc();
+    echo "<p>Current time from MySQL: " . $row[ 'current_time' ] . "</p>";
+    $result->free();
+} else {
+    echo "<p style='color:red'>Query error: " . $conn->error . "</p>";
+}
 
 $conn->close();
 ?>
