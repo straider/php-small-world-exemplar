78d7fdc90d086800285c379681d79d6c86674646
josem <JoseM@mo.co.uk>
2025-11-03 17:28:27 +0000

    Refined index PHP, with minor code beautifications.

--- src/php/index.php
+++ src/php/index.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "db";
-$username   = getenv( "MYSQL_USER"     ) ?: getenv( "MYSQL_ROOT_USER"     );
-$password   = getenv( "MYSQL_PASSWORD" ) ?: getenv( "MYSQL_ROOT_PASSWORD" );
-$database   = "demo";
+$servername = 'db';
+$username   = getenv( 'MYSQL_USER'     );
+$password   = getenv( 'MYSQL_PASSWORD' );
+$database   = 'demo';
 
 // Connect to MySQL
 $conn = new mysqli( $servername, $username, $password, $database );
 if ( $conn->connect_error ) {
-    die( "<p style='color:red'>Connection failed: " . $conn->connect_error . "</p>" );
+    die( "<p style='color:red'>Connection failed: " . $conn->connect_error . '</p>' );
 }
 
 echo '<h1>Hello, World from PHP ' . phpversion() . ' + MySQL!</h1>';
@@ -31,10 +31,10 @@ if ( extension_loaded( 'pdo_mysql' ) ) {
 $sql = "SELECT NOW() AS 'current_time'";
 if ( $result = $conn->query( $sql ) ) {
     $row = $result->fetch_assoc();
-    echo "<p>Current time from MySQL: " . $row[ 'current_time' ] . "</p>";
+    echo '<p>Current time from MySQL: ' . $row[ 'current_time' ] . '</p>';
     $result->free();
 } else {
-    echo "<p style='color:red'>Query error: " . $conn->error . "</p>";
+    echo "<p style='color:red'>Query error: " . $conn->error . '</p>';
 }
 
 $conn->close();
